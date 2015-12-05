3841c88ee31483b1af2f4d4cd54da09617715cab
GtrCraft <nickvanbokhorst@hotmail.com>
2015-12-05 17:11:24 +0100

    Fix derp

--- device-common.mk
+++ device-common.mk
@@ -24,7 +24,7 @@ PRODUCT_PROPERTY_OVERRIDES := \
     wifi.supplicant_scan_interval=15 \
     tf.enable=y \
     persist.sys.media.legacy-drm=true \
-    drm.service.enabled=true \
+    drm.service.enabled=true
 
 # libhwui flags
 PRODUCT_PROPERTY_OVERRIDES += \
